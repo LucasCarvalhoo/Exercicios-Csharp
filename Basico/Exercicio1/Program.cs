59db39167aada695581eb9b96b9490c4faca9583
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2023-01-09 22:25:15 -0300

    Primeiro exercicio c#

---
+++ Basico/Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estoque medio de peças");
+            Console.WriteLine("============================\n");
+
+            Console.Write("Quantidade Minima: ");
+            int quantidadeMinima = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade Maxima: ");
+            int quantidadeMaxima = int.Parse(Console.ReadLine());
+
+            int estoqueMedio = (quantidadeMinima + quantidadeMaxima) / 2;
+
+            Console.WriteLine($"Estoque Medio: {estoqueMedio}");
+        }
+    }
+}
